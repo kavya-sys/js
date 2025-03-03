e05caed2006baa6f9ae5ff9f3eb9abc7efa0ec18
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-03-03 18:26:08 +0000

    Data type summary

---
+++ Basics/Data.js
@@ -0,0 +1,25 @@
+//  Mainly Datatypes are of two types =>primitive and non-primitive
+// Call by value and call by refernce 
+// Primitive =>call by value =>7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+// Non primitive =>Refernce =>Array,objects,functions
+// const score=100;// we do not define the type of data type =>Dynamic language
+// const isLoggedIn=false//boolean
+// const outsideTemp=null//null
+// let userEmail;//undefined
+// const id =Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id=== anotherId);// false
+// const bigNumber=222222222222222222222n //bigInt
+// console.log(bigNumber)
+/*********** non primitive***********/
+/********ARRAYS********/
+// const heros=["shaktiman","naagraj","doga"];
+// /*******OBJECTS********/
+//  let myobj={
+//     name:"vanshika",
+//     age:22,
+//     }
+// /******Function as VAriable*********/
+// const myFunction=function(){
+//     console.log("Hello world");
+// }
