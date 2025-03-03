f30225855b072cb7f20adb5177ac91fc31bcc60d
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-03-03 18:05:23 +0000

    container.config done

--- Basics/thir.js
+++ Basics/thir.js
@@ -11,7 +11,41 @@
 // let booleanIsLoggedIn =Boolean(IsLoggedIn)
 // console.log(booleanIsLoggedIn);
 // ""=>false ; "hitesh"=>true in boolean
-let SomeNumber=33
-let stringNumber =String(SomeNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let SomeNumber=33
+// let stringNumber =String(SomeNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// **************************************operations************************
+// let value=3
+// let negValue= -value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2%3);
+// let str1="hello"
+// let str2="  hitesh"
+// let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2+2); 
+// console.log(1+2+"2");
+// console.log((3+4)*5 %3);
+// console.log(+true);//1
+// console.log(+"");//0
+// let num1, num2,num3 =2+2;
+// let gameCounter=100;
+// gameCounter++;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+/***************************comparison******************/
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.group(null> 0)//false
+// console.log(null>=0);//true
+// This is due to the comparison operator as = converts the null into number.
+// === is strict check which also check the data type
+// console.log("2"===2) //fasle 
