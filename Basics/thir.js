a0c2b4f22e377584d230d140d073d9071fd3363a
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-03-03 17:36:19 +0000

    conversion of datatype

---
+++ Basics/thir.js
@@ -0,0 +1,17 @@
+// let score ="33"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+// "33"=>33
+// "33abc"=>NAN
+//  true =>1; False=>0
+// let IsLoggedIn=1
+// let booleanIsLoggedIn =Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+// ""=>false ; "hitesh"=>true in boolean
+let SomeNumber=33
+let stringNumber =String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
