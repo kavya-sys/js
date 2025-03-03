690ccde438e6b446b6d660c3badaab03f8dc2a99
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-03-03 17:24:53 +0000

    new

--- Basics/First.js
+++ Basics/First.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const accountId=144453
+let accountEmail ="va"
+var accountPassword="bacca"
+// accountId=12222 cannot change the const.
+accountEmail="bhdb"
+accountPassword="dhbvhdncj"
+
+console.table([accountEmail,accountId,accountPassword])
+/* Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ Basics/Sec.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version 
+// alert(3+3) we are using nodejs not browser
+// console.log(3+3)
+// code readability should be high
+let name="hitesh" //string
+let age= 18  //number
+// let IsLoggedIn=>True/false //boolean
+// null standalone value
+// undefined => 
+//symbol=>unique
+//object
+// console.log(typeof "hitesh")    
+// console.log(typeof "undefined") => undefined   
+// console.log(typeof "null") =>obejct    
+
+
