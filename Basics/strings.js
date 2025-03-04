0908c4521ce425209cf9d08a9d219e6a89e01c67
Vanshika jain <140872033+kavya-sys@users.noreply.github.com>
2025-03-04 10:56:01 +0000

    Math and Number

---
+++ Basics/strings.js
@@ -0,0 +1,36 @@
+// const name ="Hitesh"
+// const repoCount=50
+
+// // console.log(name+ repoCount +"Value")//old way to concatinate
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+// const gameName= new String('hitesh');
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase);
+// // console.log(gameName.charAt(2));
+// // console.log(gameName.indexOf('t'));
+// const g=gameName.substring(0,4);
+// console.log(g);
+// const anotherId=gameName.slice(-8,4)
+// console.log(anotherId);
+// const newString="  hiytehs  "
+// console.log(newString);
+// console.log(newString.trim());
+// // in urls if we want to replace the value we can use the replace functio of string in js 
+// const url="https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('sundar'));
+// const Arr=new String('hitesh-is-best-coder')
+// console.log(Arr.split('-')); //convert to array 
+
+
+
+
+/*********************************************** */
+
+
+
+
+
+
